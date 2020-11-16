45b6adc575f028a2434aa683a7a9d4717b66e695
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-11-16 14:40:57 +0100

    detect pocketed balls

--- billard_skeleton/src/main/java/at/fhv/sysarch/lab2/physics/PhysicsEngine.java
+++ billard_skeleton/src/main/java/at/fhv/sysarch/lab2/physics/PhysicsEngine.java
@@ -1,11 +1,14 @@
 package at.fhv.sysarch.lab2.physics;
 
+import at.fhv.sysarch.lab2.game.Ball;
+import at.fhv.sysarch.lab2.game.Table;
 import org.dyn4j.dynamics.Body;
 import org.dyn4j.dynamics.World;
 import org.dyn4j.dynamics.contact.ContactListener;
 import org.dyn4j.dynamics.contact.ContactPoint;
 import org.dyn4j.dynamics.contact.PersistedContactPoint;
 import org.dyn4j.dynamics.contact.SolvedContactPoint;
+import org.dyn4j.geometry.Vector2;
 
 /**
  * @author Valentin Goronjic
@@ -51,6 +54,35 @@ public class PhysicsEngine implements ContactListener {
         if (point.isSensor()) {
             // TODO - Enough overlap?
             //  - how to use listeners?
+            if (body1.getUserData() instanceof Ball) {
+                // welt-koordinaten
+                Vector2 ballPosition = body1.getTransform().getTranslation();
+
+                // relativ zu dem, was der tisch ist -> pocket ist teil vom tisch
+                Vector2 pocketPosition = body2.getTransform().getTranslation();
+                Vector2 pocketCenter = point.getFixture2().getShape().getCenter();
+
+                // welt-koordinaten
+                Vector2 pocketInWorld = pocketPosition.add(pocketCenter);
+
+                Vector2 difference = ballPosition.difference(pocketInWorld);
+                double magnitudeDifference = difference.getMagnitude();// was für eine größenordnung
+                // ist der
+                if (magnitudeDifference <= 0.035) {
+                    System.out.println("GELOCHT");
+                }
+                // unterschied
+                // abstand kleiner als delta -> pocketed
+
+
+
+                // wieso eigene klasse -> direkt game
+                // notify listener here
+
+                // body1 is ball
+            } else {
+                // body2 is ball
+            }
         }
 
         return true;
