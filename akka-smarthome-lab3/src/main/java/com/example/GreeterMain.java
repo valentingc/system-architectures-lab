904016c0ee924561d8fef9b66a5c2de5fbd17bd3
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-11-30 13:48:35 +0100

    formatting

--- akka-smarthome-lab3/src/main/java/com/example/GreeterMain.java
+++ akka-smarthome-lab3/src/main/java/com/example/GreeterMain.java
@@ -2,29 +2,24 @@ package com.example;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
-import akka.actor.typed.javadsl.*;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
 
 public class GreeterMain extends AbstractBehavior<GreeterMain.SayHello> {
 
-    public static class SayHello {
-        public final String name;
-
-        public SayHello(String name) {
-            this.name = name;
-        }
-    }
-
     private final ActorRef<Greeter.Greet> greeter;
-
-    public static Behavior<SayHello> create() {
-        return Behaviors.setup(GreeterMain::new);
-    }
 
     private GreeterMain(ActorContext<SayHello> context) {
         super(context);
         //#create-actors
         greeter = context.spawn(Greeter.create(), "greeter");
         //#create-actors
+    }
+
+    public static Behavior<SayHello> create() {
+        return Behaviors.setup(GreeterMain::new);
     }
 
     @Override
@@ -35,9 +30,17 @@ public class GreeterMain extends AbstractBehavior<GreeterMain.SayHello> {
     private Behavior<SayHello> onSayHello(SayHello command) {
         //#create-actors
         ActorRef<Greeter.Greeted> replyTo =
-                getContext().spawn(GreeterBot.create(3), command.name);
+            getContext().spawn(GreeterBot.create(3), command.name);
         greeter.tell(new Greeter.Greet(command.name, replyTo));
         //#create-actors
         return this;
+    }
+
+    public static class SayHello {
+        public final String name;
+
+        public SayHello(String name) {
+            this.name = name;
+        }
     }
 }
