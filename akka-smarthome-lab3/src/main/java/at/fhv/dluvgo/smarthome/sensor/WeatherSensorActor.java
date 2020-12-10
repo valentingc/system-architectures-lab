d8bc4f977053f824d413a7be6aa00e8008ae61a6
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-12-10 17:26:45 +0100

    implement blinds

--- akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/sensor/WeatherSensorActor.java
+++ akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/sensor/WeatherSensorActor.java
@@ -1,22 +1,35 @@
 package at.fhv.dluvgo.smarthome.sensor;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import at.fhv.dluvgo.smarthome.actuators.ac.message.TemperatureChangedMessage;
+import at.fhv.dluvgo.smarthome.actuators.blinds.message.WeatherChangedMessage;
 import at.fhv.dluvgo.smarthome.common.WeatherType;
 import at.fhv.dluvgo.smarthome.sensor.message.EnvWeatherChangedMessage;
 
 public class WeatherSensorActor extends AbstractBehavior<EnvWeatherChangedMessage> {
     private WeatherType weather;
+    private ActorRef<WeatherChangedMessage> blinds;
 
-    public static Behavior<EnvWeatherChangedMessage> create() {
-        return Behaviors.setup(WeatherSensorActor::new);
+    public static Behavior<EnvWeatherChangedMessage> create(
+        ActorRef<WeatherChangedMessage> blinds
+    ) {
+        return Behaviors.setup(ctx -> new WeatherSensorActor(
+            ctx,
+            blinds
+        ));
     }
 
-    private WeatherSensorActor(ActorContext<EnvWeatherChangedMessage> context) {
+    private WeatherSensorActor(
+        ActorContext<EnvWeatherChangedMessage> context,
+        ActorRef<WeatherChangedMessage> blinds
+    ) {
         super(context);
+        this.blinds = blinds;
     }
 
     @Override
@@ -31,6 +44,7 @@ public class WeatherSensorActor extends AbstractBehavior<EnvWeatherChangedMessag
         weather = WeatherType.values()[msg.getWeather()];
         getContext().getLog().debug("Received new weather value");
 
+        blinds.tell(new WeatherChangedMessage(weather));
         return this;
     }
 }
