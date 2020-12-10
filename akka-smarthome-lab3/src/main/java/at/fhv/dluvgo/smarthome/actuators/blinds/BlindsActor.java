4931e852ab10790f4120e60824c5c3fa3ad54558
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-12-10 19:48:13 +0100

    Improve communication between MediaStation and Blinds

--- akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/actuators/blinds/BlindsActor.java
+++ akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/actuators/blinds/BlindsActor.java
@@ -6,10 +6,8 @@ import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 import at.fhv.dluvgo.smarthome.Message;
-import at.fhv.dluvgo.smarthome.actuators.blinds.message.CloseBlindsRequestMessage;
 import at.fhv.dluvgo.smarthome.actuators.blinds.message.MediaPlaybackStartedMessage;
 import at.fhv.dluvgo.smarthome.actuators.blinds.message.MediaPlaybackStoppedMessage;
-import at.fhv.dluvgo.smarthome.actuators.blinds.message.OpenBlindsRequestMessage;
 import at.fhv.dluvgo.smarthome.actuators.blinds.message.WeatherChangedMessage;
 import at.fhv.dluvgo.smarthome.common.WeatherType;
 import at.fhv.dluvgo.smarthome.sensor.WeatherSensorActor;
@@ -31,8 +29,6 @@ public class BlindsActor extends AbstractBehavior<Message> {
     public Receive<Message> createReceive() {
         return newReceiveBuilder()
             .onMessage(WeatherChangedMessage.class, this::onWeatherChanged)
-            .onMessage(OpenBlindsRequestMessage.class, this::onOpenBlindsRequest)
-            .onMessage(CloseBlindsRequestMessage.class, this::onCloseBlindsRequest)
             .onMessage(MediaPlaybackStartedMessage.class, this::onMediaPlaybackStarted)
             .onMessage(MediaPlaybackStoppedMessage.class, this::onMediaPlaybackStopped)
             .build();
@@ -68,18 +64,18 @@ public class BlindsActor extends AbstractBehavior<Message> {
     }
 
     /**
-     * Handles a request to open the blinds ({@link OpenBlindsRequestMessage}). If the current
-     * weather is sunny ({@link WeatherType#SUNNY}), the blinds are kept closed.
+     * Handles the information that a media playback has been started
+     * ({@link MediaPlaybackStartedMessage}). If the current weather is sunny
+     * ({@link WeatherType#SUNNY}), the blinds are kept closed.
      *
-     * @param msg The request to open the blinds
+     * @param msg The information that a playback has been started
      *
      * @return this - no change of behavior
      */
-    private Behavior<Message> onOpenBlindsRequest(OpenBlindsRequestMessage msg) {
-        if (currentWeather.equals(WeatherType.SUNNY)) {
-            getContext().getLog().debug("Keeping blinds closed - weather is {}", WeatherType.SUNNY);
-        } else {
-            this.blindsClosed = true;
+    private Behavior<Message> onMediaPlaybackStarted(MediaPlaybackStartedMessage msg) {
+        mediaPlaybackRunning = true;
+        if (!blindsClosed) {
+            blindsClosed = true;
             getContext().getLog().info("The blinds are now: closed");
         }
 
@@ -87,29 +83,22 @@ public class BlindsActor extends AbstractBehavior<Message> {
     }
 
     /**
-     * Handles a request to close the blinds ({@link CloseBlindsRequestMessage}).
+     * Handles the information that a media playback has been stopped
+     * ({@link MediaPlaybackStoppedMessage}).
      *
-     * @param msg The request to close the blinds
+     * @param msg The information that a playback has been stopped
      *
      * @return this - no change of behavior
      */
-    private Behavior<Message> onCloseBlindsRequest(CloseBlindsRequestMessage msg) {
-        if (!blindsClosed) {
-            getContext().getLog().info("The blinds are now: closed");
+    private Behavior<Message> onMediaPlaybackStopped(MediaPlaybackStoppedMessage msg) {
+        mediaPlaybackRunning = false;
+        if (currentWeather.equals(WeatherType.SUNNY)) {
+            getContext().getLog().debug("Keeping blinds closed - weather is {}", WeatherType.SUNNY);
+        } else {
+            blindsClosed = false;
+            getContext().getLog().info("The blinds are now: open");
         }
 
-        return this;
-    }
-
-    // TODO - JavaDoc
-    private Behavior<Message> onMediaPlaybackStarted(MediaPlaybackStartedMessage msg) {
-        this.mediaPlaybackRunning = true;
-        return this;
-    }
-
-    // TODO - JavaDoc
-    private Behavior<Message> onMediaPlaybackStopped(MediaPlaybackStoppedMessage msg) {
-        this.mediaPlaybackRunning = false;
         return this;
     }
 }
--- akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/actuators/blinds/message/OpenBlindsRequestMessage.java
+++
@@ -1,7 +0,0 @@
-package at.fhv.dluvgo.smarthome.actuators.blinds.message;
-
-import at.fhv.dluvgo.smarthome.Message;
-
-public class OpenBlindsRequestMessage implements Message {
-    // Intentionally empty
-}
