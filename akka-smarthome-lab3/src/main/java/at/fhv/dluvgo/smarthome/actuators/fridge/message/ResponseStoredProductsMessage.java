0b22ae546d4b0d1631902c60e7e693f3b461e750
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-12-11 18:18:46 +0100

    add request/response stored products msg (draft)

---
+++ akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/actuators/fridge/message/ResponseStoredProductsMessage.java
@@ -0,0 +1,14 @@
+package at.fhv.dluvgo.smarthome.actuators.fridge.message;
+
+import akka.actor.typed.ActorRef;
+import at.fhv.dluvgo.smarthome.actuators.fridge.FridgeActor;
+import java.util.List;
+
+public class ResponseStoredProductsMessage implements FridgeMessage {
+    public final List<FridgeActor.Product> products;
+
+    public ResponseStoredProductsMessage(List<FridgeActor.Product> products) {
+        this.products = products;
+    }
+
+}
