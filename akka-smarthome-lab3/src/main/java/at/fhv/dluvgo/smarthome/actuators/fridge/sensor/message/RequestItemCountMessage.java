e331621ad94a40e4900b0bb881b63515b79eef80
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-12-14 11:01:48 +0100

    Remove unused code

--- akka-smarthome-lab3/src/main/java/at/fhv/dluvgo/smarthome/actuators/fridge/sensor/message/RequestItemCountMessage.java
+++
@@ -1,7 +0,0 @@
-package at.fhv.dluvgo.smarthome.actuators.fridge.sensor.message;
-
-import at.fhv.dluvgo.smarthome.Message;
-
-// TODO - Determine whether needed or not
-public class RequestItemCountMessage implements Message {
-}
